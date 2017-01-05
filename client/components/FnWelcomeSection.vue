80cecd15d3c89e2f834bcd352503628a9837e6fc
thousandsofthem <thousandsofthem@gmail.com>
2017-01-05 23:50:10 +0300

    update welcome links

--- client/components/FnWelcomeSection.vue
+++ client/components/FnWelcomeSection.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="welcome-section">
+    <!--
     <h4>Get Started</h4>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
     </p>
+    -->
+    <br />
+    <br />
     <ul class="nav nav-sidebar">
-      <li><a href="#">Quick Start</a></li>
-      <li><a href="#">IronFunctions API</a></li>
-      <li><a href="#">Github</a></li>
-      <li><a href="#">Slack</a></li>
-      <li><a href="#">Report Issue</a></li>
+      <li><a href="https://github.com/iron-io/functions#quickstart" target="_blank">Quick Start</a></li>
+      <li><a href="http://petstore.swagger.io/?url=https://raw.githubusercontent.com/iron-io/functions/master/docs/swagger.yml" target="_blank">IronFunctions API</a></li>
+      <li><a href="https://github.com/iron-io/functions" target="_blank">IronFunctions Github</a></li>
+      <li><a href="https://github.com/iron-io/functions_ui" target="_blank">IronFunctionsUI Github</a></li>
+      <li><a href="http://get.iron.io/open-slack" target="_blank">Slack</a></li>
     </ul>
   </div>
 </template>
