c211a2a57d747b1d68d4a56d07ce8d1fc7d7f246
Nigel Deakin <nigel.deakin@oracle.com>
2017-09-18 17:39:13 +0100

    Extend existing chart of queued/running/completed to show per-function breakdown

--- client/client.js
+++ client/client.js
@@ -18,12 +18,36 @@ import { defaultErrorHandler, getAuthToken } from './lib/helpers';
 
 export const eventBus = new Vue();
 
+const numXValues = 50;
+
 const router = new VueRouter({
   routes: [
     { path: '/', component: IndexPage },
     { path: '/app/:appname', component: AppPage }
   ]
 });
+
+// factory for background colors; simply iterate round these arrays of colors
+//const backgroundColors = ['pink','lightgreen','yellow'];
+const backgroundColors = ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 
+                          'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)' ];
+const borderColors = ['rgba(255,99,132,1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 
+                      'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)'];
+var backgroundColorMap = {}
+var borderColorMap = {}
+
+export function getBackgroundColorFor(path){
+  if (!backgroundColorMap[path]){
+     backgroundColorMap[path]=backgroundColors[(Object.keys(backgroundColorMap).length) % (backgroundColors.length)];
+  }
+  return backgroundColorMap[path];
+}
+export function getBorderColorFor(path){
+  if (!borderColorMap[path]){
+     borderColorMap[path]=borderColors[(Object.keys(borderColorMap).length) % (borderColors.length)];
+  }
+  return borderColorMap[path];
+}
 
 new Vue({
   router: router,
@@ -50,12 +74,13 @@ new Vue({
     },
     initialiseStatshistory: function(){
       this.statshistory = [];
-      for (var i = 0; i < 50; i++) {
+      for (var i = 0; i < numXValues; i++) {
       	this.statshistory.push(
       	  {
       	    Queue: 0,
-      	    Runnning: 0,
-      	    Complete: 0
+      	    Running: 0,
+      	    Complete: 0,
+      	    FunctionStatsMap: {}
       	  }      	  
       	)
       } 
@@ -71,10 +96,25 @@ new Vue({
             t.statshistory = [statistics];
           } else {
             t.statshistory.push(statistics);
-            if (t.statshistory.length > 50){
+            if (t.statshistory.length > numXValues){
               t.statshistory.shift();
             }
+          }        
+          
+          // do the stats contain a new function?
+          var previousKnownFunctions = Object.keys(t.statshistory[t.statshistory.length-2].FunctionStatsMap);
+          var nowKnownFunctions = Object.keys(t.statshistory[t.statshistory.length-1].FunctionStatsMap);
+          for (var j = 0; j < nowKnownFunctions.length; j++){
+            if (previousKnownFunctions.indexOf(nowKnownFunctions[j])==-1){
+              var newFunction = nowKnownFunctions[j];
+              // we have a new function: backfill all the earlier stats with zero values for this function
+              for (var k = 0; k < t.statshistory.length-1; k++){
+                t.statshistory[k].FunctionStatsMap[newFunction]={Queue:0, Running:0, Complete:0};
+              }
+            }
           }
+   
+          
           // we have new stats: notify any graphs to update themselves 
           eventBus.$emit('statsRefreshed');
         },
--- client/components/QueuedGraph.vue
+++ client/components/QueuedGraph.vue
@@ -2,28 +2,32 @@
   <line-chart 
     :chart-data="datacollection"
     :options="{
+      title: {
+        display: true,
+        text: 'Queued'
+      },
       animation: {
         duration:0 // turn off annoying bouncing animation
       },
       scales: {
         yAxes: [{
-          stacked: false, // this is the default, change to true to make stacked
+          stacked: true, 
           ticks: {
             suggestedMax: 10
           }
         }]
-      }
-        
+      }     
     }"
     >
   </line-chart>
-
+  </line-chart>
 </template>
 
 <script>
   
  import LineChart from './LineChart.js';
  import { eventBus } from '../client';
+ import { getBackgroundColorFor, getBorderColorFor } from '../client'; 
  
   export default {
     components: {
@@ -44,25 +48,24 @@
     methods: {
       updateChart () {
         if (this.statshistory && this.stats){
-          this.datacollection = {
-            labels: this.statshistory.map(eachStatistic => "" ),
-            datasets: [
-              {
-                label: 'Queued: ' + this.stats.Queue,
-                backgroundColor: 'pink',
-                data: this.statshistory.map(eachStatistic => eachStatistic.Queue )
-              }, {
-                label: 'Running: ' + this.stats.Running,
-                backgroundColor: 'lightblue',
-                data: this.statshistory.map(eachStatistic => eachStatistic.Running )
-              }
-            ]
+          this.datacollection = {};
+          this.datacollection["labels"]= this.statshistory.map(eachStatistic => "" );
+          this.datacollection["datasets"]=[];
+          for (var thisPath in this.stats.FunctionStatsMap){
+            var dataSetForPath = {
+              label: thisPath + ": "+ this.stats.FunctionStatsMap[thisPath].Queue,
+              backgroundColor: getBackgroundColorFor(thisPath),
+              borderColor: getBorderColorFor(thisPath),
+              data: this.statshistory.map(eachStatistic => eachStatistic.FunctionStatsMap[thisPath].Queue)
+            };
+            this.datacollection["datasets"].push(dataSetForPath);
+             
           }
         }
       }
     },
     created: function(){
-      // handle "stats and statshistory have been refreshed"
+      // handle "stats have been refreshed"
       eventBus.$on('statsRefreshed', (app) => {
         this.updateChart();
       });    
